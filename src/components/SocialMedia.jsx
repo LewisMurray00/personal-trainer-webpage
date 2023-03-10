078bf4cec0efa9780f680e11fae80a0ff947fd01
Lewis Murray <lewisdmurray00@gmail.com>
2023-03-10 16:51:44 +0000

    Created the socialmedia section

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsTwitter, BsInstagram } from 'react-icons/bs'
+
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+        <div>
+            <BsInstagram />
+        </div>
+    </div>
+  )
+}
+
+export default SocialMedia
