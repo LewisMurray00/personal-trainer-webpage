e7c8b1d9732289208cec3b37459af10d8d0c6bc2
Lewis Murray <lewisdmurray00@gmail.com>
2023-03-11 22:43:07 +0000

    Created the About.jsx section, need to add some scss to the file and then adding some Higher Order Components

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -24,7 +24,27 @@ const abouts = [
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+        <h2 className='head-text'>
+            Here is what you can expect working with me to <span>achieve your goals</span>
+        </h2>
+
+        <div className='app__profiles'>
+            {abouts.map((about,index) => (
+                <motion.div
+                whileInView={{ opacity: 1 }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.5, type: 'tween'}}
+                className="app__profile-item"
+                key={about.title + index}
+                >
+                    <img src={about.imgUrl} alt={about.title} />
+                    <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+                    <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
+                </motion.div>
+            ))}
+        </div>
+    </>
   )
 }
 
