82c6e1ee8c937bbfaa3a2472d4844c741e5437d7
Lewis Murray <lewisdmurray00@gmail.com>
2023-03-13 23:26:47 +0000

    Added the required code for the testimonial.jsx file

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -31,7 +31,30 @@ const Testimonial = () => {
     }
 
   return (
-    <div>Testimonial</div>
+    <>
+        <div className='app__testimonial-item app__flex'>
+            
+            <img src={test.imgUrl} alt='testimonials' />
+            
+            <div className='app__testimonial-content'>
+                <p className='p-text'>{test.feedback}</p>
+                <div>
+                    <h4 className='bold-text'>{test.name}</h4>
+                    <h5 className='p-text'>{test.age}</h5>
+                </div>
+            </div>
+        </div>
+
+        <div className='app__testimonial-btns app__flex'>
+            <div className='app__flex' onClick={(()=> handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1))}>
+                <HiChevronLeft />
+            </div>
+            <div className='app__flex' onClick={(()=> handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1))}>
+              <HiChevronRight />
+            </div>
+        </div>
+
+    </>
   )
 }
 
